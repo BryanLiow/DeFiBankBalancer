d118dd19c616153bfca4aa8904dd7ecc685082cb
BryanLiow <bryan.liow.zy@gmail.com>
2023-11-12 13:53:53 +0000

    Initial commit.

---
+++ src/DeFiBankBalancer_assets/src/index.js
@@ -0,0 +1,19 @@
+import { DeFiBankBalancer } from "../../declarations/DeFiBankBalancer";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await DeFiBankBalancer.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
